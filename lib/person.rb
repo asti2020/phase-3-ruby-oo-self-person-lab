b31e05f99ac9bc2e3fb6ccef9d166528a413eba9
Astika Luintel <astild2020@gmail.com>
2022-10-12 21:44:29 -0400

    commit tree 396705f8ad64a5074dd521aa76939175b3869fab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+require "pry"
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+
+binding.pry
+    def initialize(name)
+        @name
+    end
+    def initialize(bank_account)
+        @bank_account
+    end
+
+    def print
+        puts self.name
+        puts self.bank_account
+    end
+end
+
+mero = Person.new("name")
